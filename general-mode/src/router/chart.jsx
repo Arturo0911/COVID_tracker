0ccbb12b4c28855e80b51f5e15e425eb51701f83
Arturo Negreiros Samanez <anegreiross@outlook.com>
2020-09-17 13:34:32 -0500

    added fetch json

--- general-mode/src/router/chart.jsx
+++ general-mode/src/router/chart.jsx
@@ -1,35 +1,63 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+//import axios from 'axios';
 
-export const Chart = () => {
+export const Chart = (props) => {
 
-    const APIDjango = "http://127.0.0.1:8000/general/" // Django api
+    //const APIDjango = "http://127.0.0.1:8000/general/" // Django api
+    const jsonAPI = "https://jsonplaceholder.typicode.com/posts"; // Json placeholder examples
     const [djangoApi, SetApi] = useState([]);
 
-    useEffect(()=>{
-        axios.get(APIDjango)
-            .then(res => {
-                //console.log(res);
-                SetApi(res.data)
-            })
-            .catch(err => {
-                console.log(err);
-            })
+    const FetchData = async () => {
+
+        await fetch(jsonAPI)
+            .then(response => response.json())
+            .then(response => SetApi(response))
+
+
+
+        /*let r = await fetch(jsonAPI);
+        const data = await r.json();
+        SetApi(data)*/
+    };
+    //console.log(typeof(djangoApi));
+    //console.log(djangoApi);
+    useEffect(() => {
+        FetchData();
+        /*axios.get(jsonAPI)
+            //.then((response)=> response.json())
+            .then(res => {SetApi(res.data)})
+            .catch(err => console.log(err))*/
     }, []);
 
-    //console.log(djangoApi);
-    /*fetch(APIDjango)
-        .then((response) => response.json())
-        .then((response) => SetApi(response.data))
-        .then((response)=> console.log(api));*/
+
+    console.log(djangoApi);
 
     return (
         <div>
-            <ul>
-            {djangoApi.map(apis => (
-                <li key ={apis.id} >{apis}</li>
-                ))}
-            </ul>
+            <div className="container p-4">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Id del usuario</th>
+                            <th>Id </th>
+                            <th> titulo </th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                            {
+                                djangoApi.map((element, id) => (
+                                    <tr key={id}>
+                                        <td>{element.userId}</td>
+                                        <td>{element.id}</td>
+                                        <td>{element.title}</td>
+                                    </tr>
+                                ))
+                            }
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     );
 };
