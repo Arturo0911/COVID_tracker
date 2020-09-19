86e37a7d13367c44563215a1f90da739b3cc1cd3
Arturo Negreiros Samanez <anegreiross@outlook.com>
2020-09-19 02:14:38 -0500

    finished map

---
+++ general-mode/README.md
@@ -0,0 +1,5 @@
+# Render maps using react-google-chart
+
+    First capture
+![Greeting Started](./screenshots/capture_one.png)
+![Greeting Started](./screenshots/global.png)
--- general-mode/src/router/map.jsx
+++ general-mode/src/router/map.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Chart } from 'react-google-charts';
 import './map.css'
 
 export const Map = () => {
+
+    const DjangoAPI = `http://127.0.0.1:8000/general/global/`;
+    const [country, SetCountry] = useState([]);
+    //  const AsigData = {};
+
+
     /*const MapUrl = `https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/1/0/0.mvt?access_token=pk.eyJ1IjoicGF5bG9hZDA5MTEiLCJhIjoiY2tmOGtmY2NmMDA2MzJ6bHlzanoybXMzNSJ9.X8l8mU1QNT6VBD08NaW-rw`;
 
     const Preload = () => {
@@ -10,6 +16,53 @@ export const Map = () => {
         return maping;
     }*/
 
+    useEffect(()=>{
+        fetch(DjangoAPI)
+            .then(response => response.json())
+            .then(response => SetCountry(response.data))
+            .catch(err => console.log(err))
+    },[]);
+
+
+    //console.log(typeof(country));
+    const values_ = [['Country', 'Infected']];
+    // ['Country', 'Infected'],
+
+    //console.log(country.length);
+
+    for (let i= 0; i < country.length; i++){
+        //console.log(country[i]);
+        if (country[i][0] == "United States of America" ){
+            values_.push(['United States',country[i][1]])
+        }else if(country[i][0] == "Russian Federation"){
+            values_.push(['RU',country[i][1]])
+        }
+        else if(country[i][0] == "Bolivia (Plurinational State of)"){
+            values_.push(['Bolivia',country[i][1]])
+        }
+        else if(country[i][0] == "Iran (Islamic Republic of)"){
+            values_.push(['Iran',country[i][1]])
+        }
+        else if(country[i][0] == "Syrian Arab Republic"){
+            values_.push(['Syria',country[i][1]])
+        }
+        else if(country[i][0] == "The United Kingdom"){
+            //alert('congo')
+            values_.push(['United Kingdom',country[i][1]])
+        }
+        else if(country[i][0] == "Congo"){
+            //alert('congo')
+            values_.push([' Democratic Republic of the Congo',country[i][1]])
+        }
+        else {
+            values_.push(country[i])
+        }
+
+        // Syrian Arab Republic United Kingdom
+        //Democratic Republic of the Congo
+        
+
+    }
     return (
         <div>
             <div id="Map">
@@ -18,17 +71,10 @@ export const Map = () => {
                         width={'1200px'}
                         height={'500px'}
                         chartType="GeoChart"
-                        data={[
-                            ['Country', 'Infected'],
-                            ['Germany', 200],
-                            ['United States', 300],
-                            ['Brazil', 400],
-                            ['Canada', 500],
-                            ['France', 600],
-                            ['Ecuador', 200000],
-                            ['Peru', 200000],
-                            ['RU', 700],
-                        ]}
+                        data={
+                            values_
+                        }
+                        
                         // Note: you will need to get a mapsApiKey for your project.
                         // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
                         mapsApiKey="YOUR_KEY_HERE"
